ebc6bc754450c7457d3d2590566327b8917d314b
cohalz <cohalzweb@gmail.com>
2015-05-10 13:54:01 +0900

    onClick

--- app/src/main/java/com/example/cohalz/entropy/MainActivity.java
+++ app/src/main/java/com/example/cohalz/entropy/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,10 +14,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // TextView view = (TextView)findViewById(R.id.textView0);
+        //view.setOnClickListener((View.OnClickListener) this);
     }
+   // public void onClick(View v) {
+
+    //    if (v.getId() == R.id.textView0) {
+     //       v.setBackgroundColor(111);
+       // }
+       // }
 
 
-    @Override
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
